ea8477412dcc1686592ea04fdabf745ddd00bc31
Luke Peterson <hazelnusse@gmail.com>
2021-04-15 23:31:38 -0700

    Include iostream to resolve macos linker error

--- test/reverse.test.cpp
+++ test/reverse.test.cpp
@@ -2,31 +2,33 @@
 
 #include <doctest/doctest.h>
 #include <string>
+// This should not be needed but is due to weird doctest bug, see issue #126
+#include <iostream>
 
 TEST_CASE("SimpleReverse") {
   {
     std::string input{"abc"};
     ctci::reverse(input);
-    CHECK(input == "cba");
+    CHECK(input == std::string{"cba"});
   }
   {
     std::string input{"vaginabutthole"};
     ctci::reverse(input);
-    CHECK(input == "elohttubanigav");
+    CHECK(input == std::string{"elohttubanigav"});
   }
   {
     std::string input{"taint"};
     ctci::reverse(input);
-    CHECK(input == "tniat");
+    CHECK(input == std::string{"tniat"});
   }
   {
     std::string input;
     ctci::reverse(input);
-    CHECK(input == "");
+    CHECK(input == std::string{""});
   }
   {
     std::string input{"P"};
     ctci::reverse(input);
-    CHECK(input == "P");
+    CHECK(input == std::string{"P"});
   }
 }
