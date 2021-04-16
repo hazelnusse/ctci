fc7ae11f6d1f2b27822d41002fd759c54eceb8e9
Luke Peterson <hazelnusse@gmail.com>
2021-04-16 00:01:24 -0700

    Remove doctest entirely

--- test/check_permutation.test.cpp
+++ test/check_permutation.test.cpp
@@ -6,8 +6,10 @@
 int main() {
   boost::ut::expect(ctci::check_permutation_map("12", "123") == false);
   boost::ut::expect(ctci::check_permutation_map("12", "123456") == false);
-  boost::ut::expect(ctci::check_permutation_map("1234567890", "0123456789") == true);
+  boost::ut::expect(ctci::check_permutation_map("1234567890", "0123456789") ==
+                    true);
   boost::ut::expect(ctci::check_permutation_array("12", "123") == false);
   boost::ut::expect(ctci::check_permutation_array("12", "123456") == false);
-  boost::ut::expect(ctci::check_permutation_array("1234567890", "0123456789") == true);
+  boost::ut::expect(ctci::check_permutation_array("1234567890", "0123456789") ==
+                    true);
 }
