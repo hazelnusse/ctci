90a0500d47a6d6dd1ab7cb1708b70277da12ca70
Luke Peterson <hazelnusse@gmail.com>
2021-04-16 00:01:24 -0700

    Remove doctest entirely

--- test/is_unique.test.cpp
+++ test/is_unique.test.cpp
@@ -4,18 +4,16 @@
 
 #include <boost/ut.hpp>
 
-TEST_CASE("SimpleUnique") {
+int main() {
   for (auto const s : {"", "1", "12", "0123456789"}) {
-    CHECK(ctci::is_unique_bitfield(s) == true);
-    CHECK(ctci::is_unique_brute_force(s) == true);
-    CHECK(ctci::is_unique_sort(s) == true);
+    boost::ut::expect(ctci::is_unique_bitfield(s) == true);
+    boost::ut::expect(ctci::is_unique_brute_force(s) == true);
+    boost::ut::expect(ctci::is_unique_sort(s) == true);
   }
-}
 
-TEST_CASE("SimpleNonUnique") {
   for (auto const s : {"11", "121", "Hello world!", "Heaao worad!"}) {
-    CHECK(ctci::is_unique_brute_force(s) == false);
-    CHECK(ctci::is_unique_bitfield(s) == false);
-    CHECK(ctci::is_unique_sort(s) == false);
+    boost::ut::expect(ctci::is_unique_brute_force(s) == false);
+    boost::ut::expect(ctci::is_unique_bitfield(s) == false);
+    boost::ut::expect(ctci::is_unique_sort(s) == false);
   }
 }
--- test/urlify.test.cpp
+++ test/urlify.test.cpp
@@ -3,30 +3,30 @@
 #include <boost/ut.hpp>
 #include <string>
 
-TEST_CASE("SimpleUrlifyTest") {
+int main() {
   {
     std::string input{"Mr John Smith    "};
     ctci::urlify(input, 13);
-    CHECK(input == std::string{"Mr%20John%20Smith"});
+    boost::ut::expect(input == "Mr%20John%20Smith");
   }
   {
     std::string input{"Mr   John   Smith            "};
     ctci::urlify(input, 17);
-    CHECK(input == std::string{"Mr%20%20%20John%20%20%20Smith"});
+    boost::ut::expect(input == "Mr%20%20%20John%20%20%20Smith");
   }
   {
     std::string input{"Mr   John   Smith               "};
     ctci::urlify(input, 18);
-    CHECK(input == std::string{"Mr%20%20%20John%20%20%20Smith%20"});
+    boost::ut::expect(input == "Mr%20%20%20John%20%20%20Smith%20");
   }
   {
     std::string input{" Mr   John   Smith                 "};
     ctci::urlify(input, 19);
-    CHECK(input == std::string{"%20Mr%20%20%20John%20%20%20Smith%20"});
+    boost::ut::expect(input == "%20Mr%20%20%20John%20%20%20Smith%20");
   }
   {
     std::string input{"  Mr   John   Smith                   "};
     ctci::urlify(input, 20);
-    CHECK(input == std::string{"%20%20Mr%20%20%20John%20%20%20Smith%20"});
+    boost::ut::expect(input == "%20%20Mr%20%20%20John%20%20%20Smith%20");
   }
 }
