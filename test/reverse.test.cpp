5d8391053233c476882f20dbb31c87435141fa55
Luke Peterson <hazelnusse@gmail.com>
2021-04-15 22:52:56 -0700

    Bow down to the fact that string_view is too much advanced tech for macos and windows

--- test/reverse.test.cpp
+++ test/reverse.test.cpp
@@ -1,27 +1,32 @@
 #include <ctci/reverse.hpp>
 
 #include <doctest/doctest.h>
-#include <string_view>
+#include <string>
 
 TEST_CASE("SimpleReverse") {
   {
     char input[] = "abc";
     ctci::reverse(input);
-    CHECK(std::string_view(input) == "cba");
+    CHECK(std::string(input) == "cba");
   }
   {
     char input[] = "vaginabutthole";
     ctci::reverse(input);
-    CHECK(std::string_view(input) == "elohttubanigav");
+    CHECK(std::string(input) == "elohttubanigav");
+  }
+  {
+    char input[] = "taint";
+    ctci::reverse(input);
+    CHECK(std::string(input) == "tniat");
   }
   {
     char input[] = "";
     ctci::reverse(input);
-    CHECK(std::string_view(input) == "");
+    CHECK(std::string(input) == "");
   }
   {
     char input[] = "P";
     ctci::reverse(input);
-    CHECK(std::string_view(input) == "P");
+    CHECK(std::string(input) == "P");
   }
 }
