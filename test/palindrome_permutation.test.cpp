70ef0f63ed3c86bad09803fcfce0eb0c78cb3b21
Luke Peterson <hazelnusse@gmail.com>
2021-04-16 12:19:21 -0700

    Finish palindrome permutation exercise

--- test/palindrome_permutation.test.cpp
+++ test/palindrome_permutation.test.cpp
@@ -1,17 +1,32 @@
 #include <ctci/palindrome_permutation.hpp>
 
 #include <boost/ut.hpp>
-#include <string>
 
 int main() {
-  {
-    std::string input{"Tact Coa"};
-    ctci::palindrome_permutation(input);
-    boost::ut::expect(input == "taco cat");
-  }
-  {
-    std::string input{"Tact Coa"};
-    ctci::palindrome_permutation(input);
-    boost::ut::expect(input == "atco cta");
-  }
+  using boost::ut::expect;
+  using boost::ut::test;
+  using ctci::palindrome_permutation;
+  test("empty") = []() { expect(palindrome_permutation("") == true); };
+
+  test("length1") = []() { expect(palindrome_permutation("a") == true); };
+
+  test("length2") = []() {
+    expect(palindrome_permutation("aa") == true);
+    expect(palindrome_permutation("ab") == false);
+  };
+
+  test("length3") = []() {
+    expect(palindrome_permutation("aba") == true);
+    expect(palindrome_permutation("abc") == false);
+  };
+
+  test("length4") = []() {
+    expect(palindrome_permutation("abba") == true);
+    expect(palindrome_permutation("aBba") == false);
+    expect(palindrome_permutation("bbba") == false);
+  };
+
+  test("length5") = []() { expect(palindrome_permutation("abcde") == false); };
+
+  test("length7") = []() { expect(palindrome_permutation("aabbbaa") == true); };
 }
